089d9d004b816e19bb30ff582ce6c3f7960f66df
강소연 <nebula059@naver.com>
2022-09-25 21:49:21 +0900

    Variable을 내가 이해할 수 있게 수정

--- JAVAPRA/src/VariableExam.java
+++ JAVAPRA/src/VariableExam.java
@@ -7,15 +7,23 @@ public class VariableExam {
 
         count = 20; //여러개의 값을 줄 수 있지만 마지막에 담은 값만 출력한다. 하나에 하나만 가질 수 있다.
 
+//        int count = 20;  //이라고 간단하게 적을 수 있다.
+
         System.out.println(count);
 
-//        count = 11.1; //실수는 count가 담지 못한다.
+//        count = 11.1; //count는 int로 선언되었기 때문에에러가 생긴다 실수는 count가 담지 못한다.
 
-        double avg = 11.1;
+        double avg = 11.1; //double라는 변수타입을 사용한다.
+
+        System.out.println(avg);
 
         String name = "carami"; //변수는 첫번째 단어는 소문자
 
+        System.out.println(name);
+
         int totalCount; // 두 단어를 쓴다면 두번째 단어에 대문자
+
+
 
 
     }
